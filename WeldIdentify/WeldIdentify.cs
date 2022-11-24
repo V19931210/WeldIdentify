1297e9bb7daa08c39e9c95ce7044e7e194a05877
齐全伟 <qiquanweiwei@qq.com>
2022-11-24 09:20:40 +0800

    init

---
+++ WeldIdentify/WeldIdentify.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeldIdentify
+{
+    public partial class WeldIdentify : Form
+    {
+        public WeldIdentify()
+        {
+            InitializeComponent();
+        }
+    }
+}
